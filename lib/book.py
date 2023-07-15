93765b418033cd35bbb076d5a3ad31acd82000e1
Mohammed Al Mitwalli <mohammed.almitwalli@gmail.com>
2023-07-14 21:21:19 -0700

    Complete assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count=0):
+        self.title = title
+        self.page_count = page_count
+
+    def get_author(self):
+        return self.get_author
     
-        
+    def set_author(self, author_name):
+        self.author = author_name
+    
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self, pages):
+        if type(pages) == int:
+            self._page_count = pages
+        else:
+            print("page_count must be an integer")
+    
+    page_count = property(get_page_count, set_page_count)
+
+    def get_genre(self):
+        return self.get_genre
+
+    def set_genre(self, genre):
+        self.genre = genre
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+    
+    def set_color(self, color):
+        self.color = color
+    
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size_val):
+        if type(size_val) == int:
+            self._size = size_val
+        else:
+            print("size must be an integer")
+    
+    size = property(get_size, set_size)
+
+    def set_material(self, material):
+        self.material = material
+    
+    def set_condition(self, condition):
+        self.condition = condition
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
